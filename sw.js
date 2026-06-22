2972c61006896b6bb51329930442a23db769325b
sase8123 <sase8123@gmail.com>
2026-06-22 12:43:22 +0300

    SC23 PWA gÃ¼ncellemesi: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sc23-lisans-v21";
+const CACHE = "sc23-lisans-v22";
 const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icons/icon-192.png", "icons/icon-512.png"];
 
 self.addEventListener("install", event => {
